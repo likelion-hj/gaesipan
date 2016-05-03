fab9df8b5e472cd1fcd2cecd77e81efd9bf31aad
likelion_hj <gmlwh0716@naver.com>
2016-05-03 16:53:56 +0000

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+    @post = Post.all.reverse
+  end
+  
+  def write
+    post = Post.new
+    post.title = params[:title]
+    post.content = params[:content]
+    if post.save
+       redirect_to '/home/index'
+    else render :text => post.errors.messages[:title].first
+    end
+  end
+  
+  def reply_write
+    reply = Reply.new
+    reply.content = params[:content]
+    reply.post_id = params[:id_of_post]
+    reply.save
+    redirect_to '/home/index'
+  end
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+Post.create(title: '안녕하세요', content: "첫번째 글입니다")
+Reply.create(content: "첫 리플입니다", post_id: 1)
